e35ba1561f6a0b3947753c2bd00ab589315f54d2
Akmatovedil <akmatov.ed@gmail.com>
2023-12-06 19:48:23 +0300

    fix

--- src/main/java/com/example/Neobis_Auth_Project/NeobisAuthProjectApplication.java
+++ src/main/java/com/example/Neobis_Auth_Project/NeobisAuthProjectApplication.java
@@ -3,9 +3,11 @@ package com.example.Neobis_Auth_Project;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Slf4j
+@EnableSwagger2
 public class NeobisAuthProjectApplication {
 
 	public static void main(String[] args) {
